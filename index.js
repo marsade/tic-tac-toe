62e43a3cb90a6f9e452dd63c646bfdd9ff88c0e2
Mars Adeniji <adenijifara@gmail.com>
2023-03-13 18:38:21 +0200

    feat: Add only click once

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ let box = $(".container div");
 let currentPlayer = "X";
 let isToggled = false;
 
-$(box).click(function () {
+$(box).one("click", function () {
 	isToggled = !isToggled;
 	if (isToggled == false) {
-		currentPlayer = "O";
-		$(this).text(currentPlayer);
+		if ($(this).text().length == 0) {
+			currentPlayer = "O";
+			$(this).text(currentPlayer);
+		} else {
+			$(this).text("O");
+		}
 	} else if (isToggled == true) {
-		currentPlayer = "X";
-		$(this).text(currentPlayer);
+		if ($(this).text().length == 0) {
+			currentPlayer = "X";
+			$(this).text(currentPlayer);
+		} else {
+			$(this).text("X");
+		}
 	}
 });
-
-function noEdit() {}
